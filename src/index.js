336130a3687f3be7780ab549729628a0407d71ff
jacobhall <hall.j.alexander@gmail.com>
2021-01-19 14:12:34 -0500

    Stupid import order fix in index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
 import "assets/scss/material-kit-react.scss?v=1.9.0";
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <App />,
